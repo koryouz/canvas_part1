ac9b9067412bb2d8e38ad320cdb97ff7f5a0181e
DELHAY <delhayantoine@gmail.com>
2019-03-04 14:16:04 +0100

    Ajout de canvas part1

---
+++ exercice1/assets/js/script.js
@@ -0,0 +1,44 @@
+$(function(){
+  var c = document.getElementById("housecanvas");
+  var ctx = c.getContext("2d");
+
+
+  ctx.beginPath();
+  ctx.moveTo(100,100);
+  ctx.lineTo(100,300);
+  ctx.lineTo(300,300);
+  ctx.lineTo(300,100);
+  ctx.closePath();
+  ctx.fillStyle = "lightblue";
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(100,100);
+  ctx.lineTo(200,10);
+  ctx.lineTo(300,100);
+  ctx.closePath();
+  ctx.fillStyle = "#d7823e";
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(130,150); //windows left
+  ctx.lineTo(160,150);
+  ctx.lineTo(160,180);
+  ctx.lineTo(130,180);
+  ctx.lineTo(130,150);
+  ctx.moveTo(230,150); // windows right
+  ctx.lineTo(260,150);
+  ctx.lineTo(260,180);
+  ctx.lineTo(230,180);
+  ctx.lineTo(230,150);
+  ctx.moveTo(180,300); // porte
+  ctx.lineTo(180,200);
+  ctx.lineTo(220,200);
+  ctx.lineTo(220,300);
+  ctx.closePath();
+  ctx.fillStyle = "grey";
+  ctx.fill();
+  ctx.stroke();
+});
---
+++ exercice3/assets/js/script.js
@@ -0,0 +1,34 @@
+$(function(){
+  var c = document.getElementById("carcanvas");
+  var ctx = c.getContext("2d");
+
+
+  ctx.beginPath();
+  ctx.moveTo(100,200);
+  ctx.lineTo(300,200);
+  ctx.lineTo(300,150);
+  ctx.lineTo(250,150);
+  ctx.lineTo(250,100);
+  ctx.lineTo(150,100);
+  ctx.lineTo(150,150);
+  ctx.lineTo(100,150);
+  ctx.closePath();
+  ctx.fillStyle = "#00a551";
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(130,230,30,0,Math.PI*2,true);
+  ctx.closePath();
+  ctx.fillStyle = "grey";
+  ctx.fill();
+  ctx.stroke();
+
+
+  ctx.beginPath();
+  ctx.arc(270,230,30,0,Math.PI*2,true);
+  ctx.closePath();
+  ctx.fillStyle = "grey";
+  ctx.fill();
+  ctx.stroke();
+});
